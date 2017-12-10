326b7edc632d2820c76aa864c4cab802f35822e2
Juan Cazala <cazala@mail.sfsu.edu>
2017-12-09 23:11:29 -0300

    fix install.sh

--- install.sh
+++ install.sh
@@ -1,8 +1,10 @@
-# curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.33.7/install.sh | bash
-# source ~/.nvm/nvm.sh
-# nvm install 8
-# npm install -g pm2 coin-hive-stratum
-# pm2 install pm2-logrotate
+curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.33.7/install.sh | bash
+source ~/.nvm/nvm.sh
+nvm install 8
+npm install -g pm2
+pm2 install pm2-logrotate
+npm init --yes
+npm install --save coin-hive-stratum
 cat >./proxy.js <<FILE
 const Proxy = require("coin-hive-stratum");
 const proxy = new Proxy({
