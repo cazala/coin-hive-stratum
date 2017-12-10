87b8581faa9db08bbca01801015cdcd74ac13964
Juan Cazala <cazala@mail.sfsu.edu>
2017-12-09 22:55:14 -0300

    fix file creation, comment stuff for now

--- install.sh
+++ install.sh
@@ -1,8 +1,8 @@
-curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.33.7/install.sh | bash
-source ~/.nvm/nvm.sh
-nvm install 8
-npm install -g pm2 coin-hive-stratum
-pm2 install pm2-logrotate
+# curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.33.7/install.sh | bash
+# source ~/.nvm/nvm.sh
+# nvm install 8
+# npm install -g pm2 coin-hive-stratum
+# pm2 install pm2-logrotate
 echo <<FILE
 const Proxy = require("coin-hive-stratum");
 const proxy = new Proxy({
@@ -18,8 +18,8 @@ const domain = "yourdomain.com"
 const proxy = new Proxy({
   host: "pool.supportxmr.com",
   port: 3333,
-  key: require("fs").readFileSync(`/etc/letsencrypt/live/${domain}/privkey.pem`),
-  cert: require("fs").readFileSync(`/etc/letsencrypt/live/${domain}/fullchain.pem`),
+  key: require("fs").readFileSync("/etc/letsencrypt/live/" + domain + "/privkey.pem"),
+  cert: require("fs").readFileSync("/etc/letsencrypt/live/" + domain + "/fullchain.pem"),
 });
 proxy.listen(443);
 FILE > proxy_secure.js
