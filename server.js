42bd893a4d133e860472f8da1e15ac1d52aa6845
Juan Cazala <juancazala@gmail.com>
2018-02-03 15:45:11 -0300

    added static assets

--- server.js
+++
@@ -1,5 +0,0 @@
-const Proxy = require("./build");
-const proxy = new Proxy({
-  dynamicPool: true
-});
-proxy.listen(process.env.PORT || 8892);
--- src/index.ts
+++
@@ -1,7 +0,0 @@
-import Proxy from "./Proxy";
-export = Proxy;
-
-process.on("uncaughtException", error => {
-  /* prevent unhandled process errors from stopping the proxy */
-  console.error("process error:", error.message);
-});
